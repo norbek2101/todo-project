d849f16fae64e8186336f3af47d67a8e2922063d
norbek <lightnight1223@gmail.com>
2021-10-30 01:09:29 +0500

    ---

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,28 @@
+from functools import update_wrapper
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    target = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_completed = models.BooleanField(default=False)
+    choice_color = [
+        ('R', 'red'),
+        ('G', 'green'),
+        ('Y', 'yellow'),
+        ('B', 'blue'),
+        ('O', 'orange'), 
+    ]
+    type  = models.CharField(
+        max_length=1,
+        choices= choice_color,
+        default='R',
+    )
+
+    def __str__(self):
+        return  self.target
+
+    class Meta:
+        verbose_name = 'Todo'
+        verbose_name_plural = 'Todos'    
+
